6e62fe9f03c8f8aff1e9ecd4832b3ba9ddfdfadd
thestarweb <597577939@qq.com>
2022-02-06 22:24:38 +0800

    更新

---
+++ helper/build-star-mix.ts
@@ -0,0 +1,28 @@
+import { writeFileSync } from "fs";
+import path from "path";
+import { write } from '@thestarweb/trove-lang-tool';
+import { mapDir, readTxtFile } from "./common";
+import config from "./common/config";
+
+(async () => {
+    const LANG_DIR = "lang-txt/mix-star";
+    const BASE_DIR = "./lang-txt/en-base";
+    mapDir(BASE_DIR,(filename) => {
+        const baseEN = readTxtFile(path.join(BASE_DIR, filename));
+        const mixMap = new Map();
+        const cnMap = new Map();
+        readTxtFile(path.join(LANG_DIR, filename)).forEach(item => mixMap.set(item.key,item.value));
+        readTxtFile(path.join('./lang-txt/cn-liulianf', filename)).forEach(item => cnMap.set(item.key,item.value));
+        const newData = baseEN.map((item) => {
+            if(mixMap.get(item.key)){
+                return {key: item.key, value: mixMap.get(item.key)};
+            }
+            if(item.value === cnMap.get(item.key) || !cnMap.has(item.key)) return item;
+            const n = (cnMap.get(item.key).indexOf("\\n") != -1 || item.value.indexOf("\\n") != -1 || cnMap.get(item.key).length > 100 || item.value.length > 150) ? "\n" : "";
+            return {key: item.key, value: `${cnMap.get(item.key)}${n}(${item.value})`};
+        }).map(({key, value}) => ({key, value: value.replace(/(\r|\n)/g, "").replace(/\\n/g, "\n")}));
+        if(newData.length > 0){
+            writeFileSync(path.join(config.buildOutputDir,filename.substr(0,filename.lastIndexOf("."))+".binfab"),write(newData));
+        }
+    });
+})();
---
+++ helper/common/index.ts
@@ -0,0 +1,60 @@
+import path from 'path';
+import fs from 'fs';
+import { read } from "@thestarweb/trove-lang-tool";
+import type { LangInfoItem } from "@thestarweb/trove-lang-tool";
+
+type fileCallBack<T> = (filename:string, index:number, length:number) => T;
+
+export function mapDir<T=unknown>(dir:string, callback:fileCallBack<T>):T[]{
+    if(!fs.existsSync(dir)) return [];
+    const dirData = fs.readdirSync(dir);
+    return dirData.map((filename, index) => {
+        return callback(filename, index, dirData.length);
+    });
+}
+
+type readOption = {
+    befroeRead?: fileCallBack<unknown>;
+    afterRead?: fileCallBack<unknown>;
+}
+
+const filterSymbol = Symbol();
+export function mapBinfabDir<T=unknown>(
+    dir:string,
+    callback:(filename:string, data:LangInfoItem[], index:number, length:number) => T,
+    option:readOption={},
+):T[]{
+    return mapDir(dir, (filename:string, index:number, length:number):T|Symbol => {
+        if(filename.endsWith(".binfab")){
+            const filePath = path.join(dir, filename);
+            option.befroeRead && option.befroeRead(filename, index, length);
+            const data = callback(filename, read(fs.readFileSync(filePath)), index, length);
+            option.afterRead && option.afterRead(filename, index, length);
+            return data;
+        }
+        return filterSymbol;
+    }).filter(item => item !== filterSymbol) as T[];
+}
+export function binfabs2Txts(srcdir: string, outDir: string, option:readOption = {}):void{
+    mapBinfabDir(srcdir, (filename:string, data:LangInfoItem[]) => {
+        const outName = filename.substr(0, filename.lastIndexOf(".")) + ".txt";
+        fs.writeFileSync(path.join(outDir, outName), data.map((item) => `${item.key}=${item.value.replace(/\r/g,"").replace(/\n/g,"\\n")}`).join("\n"));
+    }, option);
+}
+export function binfabs2TxtsWithLog(srcdir: string, outDir: string = './'):void{
+    mapBinfabDir(srcdir, (filename:string, data:LangInfoItem[]) => {
+        const outName = filename.substr(0, filename.lastIndexOf(".")) + ".txt";
+        fs.writeFileSync(path.join(outDir, outName), data.map((item) => `${item.key}=${item.value.replace(/\r/g,"").replace(/\n/g,"\\n")}`).join("\n"));
+    }, {
+        befroeRead: (filename) => console.log(`转换${filename}`),
+        afterRead: (filename, index, length) => console.log(`转换${filename}完成 （${index+1}/${length}）`),
+    });
+}
+
+export function readTxtFile(path:string):LangInfoItem[]{
+    if(!fs.existsSync(path)) return [];
+    return fs.readFileSync(path, {encoding: 'utf8'}).split("\n").map(item => {
+        const [key, value] = item.split("=", 2);
+        return {key, value};
+    });
+}
